3350e5b57a08c902a77c5dd324c6e2cdc87b821d
tun027 <rebecca.tung@postgrad.curtin.edu.au>
2015-11-10 14:12:45 +0800

    working redbackbase file

---
+++ src/base/RedbackRandomFunction.h
@@ -0,0 +1,41 @@
+/****************************************************************/
+/*               DO NOT MODIFY THIS HEADER                      */
+/* MOOSE - Multiphysics Object Oriented Simulation Environment  */
+/*                                                              */
+/*           (c) 2010 Battelle Energy Alliance, LLC             */
+/*                   ALL RIGHTS RESERVED                        */
+/*                                                              */
+/*          Prepared by Battelle Energy Alliance, LLC           */
+/*            Under Contract No. DE-AC07-05ID14517              */
+/*            With the U. S. Department of Energy               */
+/*                                                              */
+/*            See COPYRIGHT for full restrictions               */
+/****************************************************************/
+
+#ifndef REDBACKRANDOMFUNCTION_H
+#define REDBACKRANDOMFUNCTION_H
+
+#include "Function.h"
+
+class RedbackRandomFunction;
+
+template<> InputParameters validParams<RedbackRandomFunction>();
+
+/**
+ * Class that represents random function
+ */
+class RedbackRandomFunction : public Function
+{
+public:
+  RedbackRandomFunction(const InputParameters & parameters);
+
+  virtual Real value(Real t, const Point & p);
+
+protected:
+  const Real & _value;
+};
+
+#endif
+
+
+
